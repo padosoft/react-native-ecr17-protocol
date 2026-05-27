567f9e21f3f89229fb8cec4dbd99e0e784c81eee
47PADO47 <62028267+47PADO47@users.noreply.github.com>
2026-05-27 18:51:12 +0200

    chore: basic ts module

--- package/src/specs/ecr17.nitro.ts
+++
@@ -1,5 +0,0 @@
-import type { HybridObject } from 'react-native-nitro-modules'
-
-export interface Ecr17 extends HybridObject<{ ios: 'c++', android: 'c++' }> {
-  sum(num1: number, num2: number): number
-}
